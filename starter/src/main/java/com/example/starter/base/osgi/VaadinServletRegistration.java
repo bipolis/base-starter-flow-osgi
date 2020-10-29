801961abcc10bbc8bf14cdaa16b5e4e9f445fd61
Stefan Bischof <stbischof@bipolis.org>
2020-10-29 17:55:58 +0100

    changes to run
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- starter/src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
+++ starter/src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
@@ -6,14 +6,16 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
+import com.vaadin.flow.di.ResourceProvider;
+import com.vaadin.flow.server.InitParameters;
+import com.vaadin.flow.server.VaadinServlet;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.http.NamespaceException;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
-
-import com.vaadin.flow.server.InitParameters;
-import com.vaadin.flow.server.VaadinServlet;
 
 /**
  * Register a VaadinServlet via HTTP Whiteboard specification
@@ -21,36 +23,36 @@ import com.vaadin.flow.server.VaadinServlet;
 @Component(immediate = true)
 public class VaadinServletRegistration {
 
-    private static final String INIT_PARAMETER_PREFIX = "servlet.init.";
+	@Reference(cardinality = ReferenceCardinality.MANDATORY)
+	ResourceProvider pr;
 
-    /**
-     * This class is a workaround for #4367. This will be removed after the
-     * issue is fixed.
-     */
-    private static class FixedVaadinServlet extends VaadinServlet {
-        @Override
-        public void init(ServletConfig servletConfig) throws ServletException {
-            super.init(servletConfig);
+	private static final String INIT_PARAMETER_PREFIX = "servlet.init.";
 
-            getService().setClassLoader(getClass().getClassLoader());
-        }
+	/**
+	 * This class is a workaround for #4367. This will be removed after the issue is
+	 * fixed.
+	 */
+	private static class FixedVaadinServlet extends VaadinServlet {
 
-    }
+		@Override
+		public void init(ServletConfig servletConfig) throws ServletException {
 
-    @Activate
-    void activate(BundleContext ctx) throws NamespaceException {
-        Hashtable<String, Object> properties = new Hashtable<>();
-        properties.put(
-                HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_ASYNC_SUPPORTED,
-                true);
-        properties.put(
-                INIT_PARAMETER_PREFIX
-                        + InitParameters.SERVLET_PARAMETER_COMPATIBILITY_MODE,
-                Boolean.FALSE.toString());
-        properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN,
-                "/*");
-        ctx.registerService(Servlet.class, new FixedVaadinServlet(),
-                properties);
-    }
+			super.init(servletConfig);
+
+			getService().setClassLoader(getClass().getClassLoader());
+		}
+
+	}
+
+	@Activate
+	void activate(BundleContext ctx) throws NamespaceException {
+
+		Hashtable<String, Object> properties = new Hashtable<>();
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_ASYNC_SUPPORTED, true);
+		properties.put(INIT_PARAMETER_PREFIX + InitParameters.SERVLET_PARAMETER_COMPATIBILITY_MODE,
+				Boolean.FALSE.toString());
+		properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN, "/*");
+		ctx.registerService(Servlet.class, new FixedVaadinServlet(), properties);
+	}
 
 }
